d29b7c4d848dd252b57976b0bcf4a6e60edd18c9
mgernand <m.gernand@mge-it.de>
2013-03-09 03:13:35 +0100

    Changed visibility.

--- Source/ExpressionReflect/ExpressionReflector.cs
+++ Source/ExpressionReflect/ExpressionReflector.cs
@@ -2,9 +2,9 @@
 {
 	using System.Linq.Expressions;
 
-	public static class ExpressionReflector
+	internal static class ExpressionReflector
 	{
-		public static object Execute(Expression expression, params object[] values)
+		internal static object Execute(Expression expression, params object[] values)
 		{
 			ReflectionOutputExpressionVisitor visitor = new ReflectionOutputExpressionVisitor(expression, values);
 			object result = visitor.Execute();
